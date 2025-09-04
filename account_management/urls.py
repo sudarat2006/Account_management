9ecdea39cbe073724dd553e5846bdba02b053755
phattharawadee24 <phattharawadeess283@gmail.com>
2025-09-05 00:44:41 +0700

    p

--- account_management/urls.py
+++ account_management/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- account_management/views.py
+++ account_management/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('profile')
+        else:
+            messages.error(request, 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def register(request):
+    return render(request, 'register.html')
+
+def profile(request):
+    return render(request, 'profile.html')
