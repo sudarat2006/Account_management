fd3ed463b63d3aac6e8b94bb8ae6439c48b6a94b
sudarat2006 <sudarat.wanthawee@gmail.com>
2025-09-04 23:28:23 +0700

    Add base layout and index page for Modern WebApp
    - Created a base HTML template with Bootstrap 5 styling and responsive design.
    - Implemented a hero section and feature cards in the index page to showcase available functionalities.
    - Added navigation links for easy access to different features of the application.

--- account/settings.py
+++ account/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@s4=xp+!^p31hg%*(or!9@6_ei#e$27-q&#6dgrcx*za$(trbj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account_management',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'account.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
